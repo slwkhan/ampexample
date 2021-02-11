e4ca809c5f8163139dbcac0618e18e52b6dc8f25
slwkhan <78135238+slwkhan@users.noreply.github.com>
2021-02-10 19:34:26 -0500

    Update ampexample.js

--- ampexample.js
+++ ampexample.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/amp/qualtrics/qamp.js'], function(APIConstructor, ampExtension){
+define(['pipAPI', 'https://slwkhan.github.io/ampexample/ampexample-full.js'], function(APIConstructor, ampExtension){
 
 	var API = new APIConstructor();
 	
